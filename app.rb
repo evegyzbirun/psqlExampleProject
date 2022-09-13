b3851d222419aa97ea1d5a608f53e47d93d8d6bb
James Perry <PjPerry28@gmail.com>
2022-09-12 21:08:38 -0700

    add delete method

--- app.rb
+++ app.rb
@@ -25,3 +25,27 @@ post('/albums') do
   @albums = Album.all() 
   erb(:albums)
 end
+
+get('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
+end
+
+get('/albums/:id/edit') do
+  @album = Album.find(params[:id].to_i())
+  erb(:edit_album)
+end
+
+patch('/albums/:id') do 
+  if params[:name] != ""
+    @album = Album.find(params[:id].to_i())
+    @abum.update(params[:name])
+  end
+  @albums = Album.allerb(:albums)
+end
+delete('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @albums.delete()
+  @album = Album.all
+  erb(:albums)
+end
